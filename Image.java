328632bab7c72fbf9894aeab8bee9ecc12c04940
Robert <robertdragoi20@gmail.com>
2021-11-17 11:33:09 +0200

    Fix issues from feedback

--- Image.java
+++ Image.java
@@ -1,13 +1,8 @@
-import java.util.ArrayList; 
 
 public class Image implements Element{
     String imageName;
-    ArrayList<Element> elements=new ArrayList<Element>();
     public Image(String imageName) {
         this.imageName =imageName;
-    }
-    public void add(Element element){
-        this.elements.add(element);
     }
     public void print(){
         System.out.println("Image with name: "+this.imageName);
